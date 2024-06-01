cb326d09b1ea540fecdba919cfb5dedaf10e2ff6
Khalisha Dzakira <khalishadzakira@gmail.com>
2024-06-02 01:01:43 +0700

    api detection

--- src/controllers/detection.controller.js
+++ src/controllers/detection.controller.js
@@ -1,4 +1,31 @@
-const predictClassification = require('../services/inferenceService');
+const predictDetection = require('../services/inferenceService'); 
 const crypto = require('crypto');
 const storeData = require('../services/storeData');
 const { Firestore } = require('@google-cloud/firestore');
+
+async function postDetectHandler(request, h) {
+  const { image } = request.payload;
+  const { model } = request.server.app;
+  
+  const { result, suggestion } = await predictDetection(model, image);
+  const id = crypto.randomUUID();
+  const createdAt = new Date().toISOString();
+
+  const data = {
+    id: id,
+    result: result,
+    suggestion: suggestion,
+    createdAt: createdAt
+  };
+
+  await storeData(id, data);
+
+  const response = h.response({
+    status: 'success',
+    message: 'Detection model predicted successfully',
+    data: data
+  });
+
+  response.code(201);
+  return response;
+}
