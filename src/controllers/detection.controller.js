f0da4a52ccde139b1c19a72fd5225ee662b38f8f
Khalisha Dzakira <khalishadzakira@gmail.com>
2024-06-02 00:45:52 +0700

    testing

---
+++ src/controllers/detection.controller.js
@@ -0,0 +1,4 @@
+const predictClassification = require('../services/inferenceService');
+const crypto = require('crypto');
+const storeData = require('../services/storeData');
+const { Firestore } = require('@google-cloud/firestore');
