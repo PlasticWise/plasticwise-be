d582f09c74e0890319c45361e5a7e5c1eb7c72ea
Khalisha Dzakira <khalishadzakira@gmail.com>
2024-06-02 01:04:00 +0700

    api detection

--- src/controllers/detection.controller.js
+++ src/controllers/detection.controller.js
@@ -1,31 +1,41 @@
-const predictDetection = require('../services/inferenceService'); 
+const predictDetection = require('../services/inferenceService');
 const crypto = require('crypto');
 const storeData = require('../services/storeData');
-const { Firestore } = require('@google-cloud/firestore');
 
 async function postDetectHandler(request, h) {
   const { image } = request.payload;
   const { model } = request.server.app;
-  
-  const { result, suggestion } = await predictDetection(model, image);
-  const id = crypto.randomUUID();
-  const createdAt = new Date().toISOString();
 
-  const data = {
-    id: id,
-    result: result,
-    suggestion: suggestion,
-    createdAt: createdAt
-  };
+  try {
+    const { result, suggestion } = await predictDetection(model, image);
+    const id = crypto.randomUUID();
+    const createdAt = new Date().toISOString();
 
-  await storeData(id, data);
+    const data = {
+      id: id,
+      result: result,
+      suggestion: suggestion,
+      createdAt: createdAt
+    };
 
-  const response = h.response({
-    status: 'success',
-    message: 'Detection model predicted successfully',
-    data: data
-  });
+    await storeData(id, data);
 
-  response.code(201);
-  return response;
+    const response = h.response({
+      status: 'success',
+      message: 'Detection model predicted successfully',
+      data: data
+    });
+
+    response.code(201);
+    return response;
+  } catch (error) {
+    const response = h.response({
+      status: 'fail',
+      message: error.message
+    });
+    response.code(500);
+    return response;
+  }
 }
+
+module.exports = { postDetectHandler };
